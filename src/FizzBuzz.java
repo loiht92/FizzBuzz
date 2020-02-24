fc7dca23e64004f43645d80bcdb54aa6bc20a488
loiminh <loiho11082015@gmail.com>
2020-02-24 10:13:56 +0700

    Add file

---
+++ src/FizzBuzz.java
@@ -0,0 +1,17 @@
+public class FizzBuzz {
+    public static String fizzBuzz(int number){
+
+        boolean isFuzz = number % 3 == 0;
+        boolean isBuzz = number % 5 == 0;
+        if(isFuzz && isBuzz)
+            return "FizzBuzz";
+
+        if(isFuzz)
+            return "Fizz";
+
+        if(isBuzz)
+            return "Buzz";
+
+        return number + "";
+    }
+}
---
+++ src/Test.java
@@ -0,0 +1,7 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(FizzBuzz.fizzBuzz(4));
+        System.out.println(FizzBuzz.fizzBuzz(3));
+        System.out.println(FizzBuzz.fizzBuzz(6));
+    }
+}
